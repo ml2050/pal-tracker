b03dc0fdf47ff2506aa357d88d0709f1738b6fc1
Munir <aa@bb>
2020-10-08 16:35:17 +0000

    implement the database version of the Time Entry Repository using Spring JDBC Template.

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,18 +1,30 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
+    //@Autowired
+    //private DataSource datasource;
 
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 
+
+//    @Bean
+//        TimeEntryRepository timeEntryRepository() {
+//               return new InMemoryTimeEntryRepository();
+//           }
+
+
     @Bean
-    TimeEntryRepository timeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
+    TimeEntryRepository timeEntryRepository(DataSource dataSource) {
+        return new JdbcTimeEntryRepository(dataSource);
     }
 }
