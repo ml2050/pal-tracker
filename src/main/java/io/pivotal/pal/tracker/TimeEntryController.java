e1991c11a2209f29b7385b05eaf0572f44a07aa9
Munir <aa@bb>
2020-10-08 20:01:27 +0000

    This lab introduces Spring Boot Actuator, a library that adds many production-ready monitoring features to an app.

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -1,5 +1,8 @@
 package io.pivotal.pal.tracker;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,20 +14,28 @@ import java.util.List;
 public class TimeEntryController {
 
     private final TimeEntryRepository timeEntryRepository;
-    public TimeEntryController(TimeEntryRepository timeEntryRepository) {
+    private final DistributionSummary timeEntrySummary;
+    private final Counter actionCounter;
+    public TimeEntryController(TimeEntryRepository timeEntryRepository, MeterRegistry meterRegistry) {
         this.timeEntryRepository = timeEntryRepository;
+        timeEntrySummary = meterRegistry.summary("timeEntry.summary");
+        actionCounter = meterRegistry.counter("timeEntry.actionCounter");
     }
+
 
     @PostMapping
     public ResponseEntity<TimeEntry> create(@RequestBody TimeEntry timeEntryToCreate) {
     TimeEntry createdTimeEntry = timeEntryRepository.create(timeEntryToCreate);
-        return new ResponseEntity(createdTimeEntry, HttpStatus.CREATED);
+        actionCounter.increment();
+        timeEntrySummary.record(timeEntryRepository.list().size());
+    return new ResponseEntity(createdTimeEntry, HttpStatus.CREATED);
     }
 
     @GetMapping("{id}")
     public ResponseEntity<TimeEntry> read(@PathVariable("id") long timeEntryId) {
         TimeEntry findTimeEntry = timeEntryRepository.find(timeEntryId);
         if (findTimeEntry != null) {
+            actionCounter.increment();
             return new ResponseEntity(findTimeEntry, HttpStatus.OK);
         } else {
             return new ResponseEntity(findTimeEntry, HttpStatus.NOT_FOUND);
@@ -32,6 +43,7 @@ public class TimeEntryController {
     }
     @GetMapping
     public ResponseEntity<List<TimeEntry>> list() {
+        actionCounter.increment();
         return new ResponseEntity(timeEntryRepository.list(), HttpStatus.OK);
     }
     @PutMapping("{id}")
@@ -39,6 +51,7 @@ public class TimeEntryController {
 
         TimeEntry updatedTimeEntry = timeEntryRepository.update(timeEntryId, expected);
         if (updatedTimeEntry != null) {
+            actionCounter.increment();
             return new ResponseEntity(updatedTimeEntry, HttpStatus.OK);
         } else {
             return new ResponseEntity(HttpStatus.NOT_FOUND);
@@ -47,7 +60,7 @@ public class TimeEntryController {
     @DeleteMapping("{id}")
     public ResponseEntity delete(@PathVariable("id") long timeEntryId) {
         timeEntryRepository.delete(timeEntryId);
-
+        actionCounter.increment();
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
